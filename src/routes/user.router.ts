2cccef267298a5dcc3310039b09c3c0d0cf27f81
Moritz Oman <moritz.oman@htl-wels.at>
2024-06-27 14:00:36 +0200

    feat: post schema wird ueberprueft und model file erstellt

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,9 +1,36 @@
 import express from "express";
+import { createUserSchema } from "../schema/user.schema";
 
 const router = express.Router();
 
 router.get('/', (request, response) =>{
     response.status(200).send("hello world user");
 });
+
+router.post('/', (req, res) =>{
+    const validated = createUserSchema.safeParse(req.body);
+
+    if(validated.success){
+        res.status(200).json(req.body);
+    
+    }else{
+        res.status(400).json({
+            error: "Malfromed request body"
+        })
+    }
+
+    /**
+     * We're expecting the following body:
+     * {
+     *  username: string,
+     *  email: string,
+     *  password: string
+     * }
+     */
+
+    console.log(req.body);
+
+    res.status(200).json(req.body);
+})
 
 export default router;
