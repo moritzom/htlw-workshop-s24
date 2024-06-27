62877fcb548f578b9a3cb41391b847db5d2b411e
Moritz Oman <moritz.oman@htl-wels.at>
2024-06-27 12:30:01 +0200

    feat: cors hinzugefuegt

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import envSchema  from './util/env.util';
 import express, { response } from 'express';
 import router from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/", router);
 
